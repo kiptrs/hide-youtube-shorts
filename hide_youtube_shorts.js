be39a09ee11d504f9957c60817e05310be026326
kiprastar <kiprastar@gmail.com>
2022-09-07 22:08:40 +0300

    init

---
+++ hide_youtube_shorts.js
@@ -0,0 +1,15 @@
+setInterval(() => hideShorts(), 5000);
+setTimeout(() => {
+  hideShorts();
+}, 1000);
+setTimeout(() => {
+  hideShorts();
+}, 1500);
+setTimeout(() => {
+  hideShorts();
+}, 2000);
+const hideShorts = () => {
+  document.querySelectorAll('[overlay-style="SHORTS"]').forEach((element) => {
+    element.parentElement.parentElement.parentElement.parentElement.remove();
+  });
+};
